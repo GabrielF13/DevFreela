561a8a5e014fcada4dc8e49ac0f5325960d0bd42
Gabriel Ferreira <gabrielferreiracunha@gmail.com>
2024-04-16 20:25:24 -0300

    FEAT: Adicionar projeto Domain

---
+++ DevFreela.Core/Entities/ProjectComment.cs
@@ -0,0 +1,19 @@
+﻿namespace DevFreela.Core.Entities
+{
+    public class ProjectComment : BaseEntity
+    {
+        public ProjectComment(string content, int idProject, int idUser)
+        {
+            Content = content;
+            IdProject = idProject;
+            IdUser = idUser;
+
+            CreatedAt = DateTime.Now;
+        }
+
+        public string Content { get; private set; }
+        public int IdProject { get; private set; }
+        public int IdUser { get; private set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
