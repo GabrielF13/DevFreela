886ea84a12069632211a95060e75fdf55ed0b60a
Gabriel Ferreira <gabrielferreiracunha@gmail.com>
2024-04-16 21:35:42 -0300

    Style: Formatar Codigo

--- DevFreela.Infrastructure/CloudServices/Interfaces/IFileStorageService.cs
+++ DevFreela.Infrastructure/CloudServices/Interfaces/IFileStorageService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DevFreela.Infrastructure.CloudServices.Interfaces
+﻿namespace DevFreela.Infrastructure.CloudServices.Interfaces
 {
     public interface IFileStorageService
     {
